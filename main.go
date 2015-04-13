f03b608955fa4a0076de5506eeadea8dd3f40ebb
Kristian Bendiksen <kristian.bendiksen@gmail.com>
2015-04-13 15:54:31 +0200

    Push untappd checkins to a irc channel.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/kriben/untappd"
+	"github.com/nickvanw/ircx"
+	"github.com/sorcix/irc"
 	"io/ioutil"
 	"log"
 	"time"
@@ -13,11 +15,16 @@ type Config struct {
 	ClientId     string `json:"client_id"`
 	ClientSecret string `json:"client_secret"`
 	Users        []User
+	BotName      string `json:"bot_name"`
+	Server       string
+	Channel      string
 }
 
 type User struct {
 	Name string
 }
+
+var config Config
 
 func readConfigFile(fileName string) (Config, error) {
 	body, err := ioutil.ReadFile(fileName)
@@ -43,25 +50,61 @@ func isCheckinNew(checkin *untappd.Checkin, lastCheckinTimes map[string]time.Tim
 	return checkin.Created.After(lastCheckinTime)
 }
 
-func printCheckin(checkin *untappd.Checkin) {
-	fmt.Printf("%s  %s  %s  %s  %0.1f %s %0.1f %+v\n",
+func formatCheckin(checkin *untappd.Checkin) string {
+	return fmt.Sprintf("untappd alert: %s had %s (%s). Style: %s Rating: %0.1f \"%s\"\n",
 		checkin.User.UserName,
 		checkin.Beer.Name,
 		checkin.Brewery.Name,
 		checkin.Beer.Style,
-		checkin.Beer.ABV,
-		checkin.Comment,
 		checkin.UserRating,
-		checkin.Created,
+		checkin.Comment,
 	)
 }
 
 func main() {
-	config, err := readConfigFile("./config.json")
+	var err error
+	config, err = readConfigFile("./config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	bot := ircx.Classic(config.Server, config.BotName)
+	if err := bot.Connect(); err != nil {
+		log.Panicln("Unable to dial IRC Server ", err)
+	}
+
+	RegisterHandlers(bot)
+	bot.CallbackLoop()
+	log.Println("Exiting..")
+}
+
+func RegisterHandlers(bot *ircx.Bot) {
+	bot.AddCallback(irc.RPL_WELCOME, ircx.Callback{Handler: ircx.HandlerFunc(RegisterConnect)})
+	bot.AddCallback(irc.PING, ircx.Callback{Handler: ircx.HandlerFunc(PingHandler)})
+	bot.AddCallback(irc.RPL_NAMREPLY, ircx.Callback{Handler: ircx.HandlerFunc(JoinedHandler)})
+}
+
+func RegisterConnect(s ircx.Sender, m *irc.Message) {
+	s.Send(&irc.Message{
+		Command: irc.JOIN,
+		Params:  []string{config.Channel},
+	})
+}
+
+func PingHandler(s ircx.Sender, m *irc.Message) {
+	s.Send(&irc.Message{
+		Command:  irc.PONG,
+		Params:   m.Params,
+		Trailing: m.Trailing,
+	})
+}
+
+func JoinedHandler(s ircx.Sender, m *irc.Message) {
+	log.Printf("Joined channel %s.", config.Channel)
+	go untappdLoop(s)
+}
+
+func untappdLoop(s ircx.Sender) {
 	client, err := untappd.NewClient(
 		config.ClientId,
 		config.ClientSecret,
@@ -74,7 +117,7 @@ func main() {
 
 	lastCheckinTimes := make(map[string]time.Time)
 	for {
-		fmt.Printf("Checking %d users.\n", len(config.Users))
+		log.Printf("Checking %d users.\n", len(config.Users))
 		for _, user := range config.Users {
 			checkins, _, err := client.User.Checkins(user.Name)
 			if err != nil {
@@ -83,7 +126,12 @@ func main() {
 
 			for _, c := range checkins {
 				if isCheckinNew(c, lastCheckinTimes) {
-					printCheckin(c)
+					message := formatCheckin(c)
+					s.Send(&irc.Message{
+						Command:  irc.PRIVMSG,
+						Params:   []string{config.Channel},
+						Trailing: message,
+					})
 				}
 			}
 
@@ -91,7 +139,6 @@ func main() {
 				lastCheckinTimes[checkins[0].User.UserName] = checkins[0].Created
 			}
 		}
-		fmt.Printf("%+v\n", lastCheckinTimes)
-		time.Sleep(1 * time.Minute)
+		time.Sleep(5 * time.Minute)
 	}
 }
